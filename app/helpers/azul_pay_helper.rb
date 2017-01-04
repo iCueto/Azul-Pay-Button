f46e98f557238e540f2aa6e5d73e7e5116d2f88c
Alex Cueto <cuetodev@gmail.com>
2017-01-04 01:20:37 -0400

    finished the helper method 🙌🏼

--- app/helpers/azul_pay_helper.rb
+++ app/helpers/azul_pay_helper.rb
@@ -1,27 +1,32 @@
+require 'digest'
+
 module AzulPayHelper
 
   def azul_pay_button(order_number, currency, amount, itbis)
+    amount = amount + itbis
+
+    auth_hash =  AzulPayButton.config.merchant_id + AzulPayButton.config.merchant_name + AzulPayButton.config.merchant_type + currency +  order_number + amount + AzulPayButton.config.approved_url + AzulPayButton.config.declined_url + AzulPayButton.config.cancel_url + AzulPayButton.config.response_post_url + AzulPayButton.config.custom_field_1 + AzulPayButton.config.custom_field_1_label + AzulPayButton.config.custom_field_1_value + AzulPayButton.config.custom_field_2 + AzulPayButton.config.custom_field_2_label + AzulPayButton.config.custom_field_2_value + AzulPayButton.config.auth_key
 
     #form for Pay
-    "<form action=\"http://urldelpaymentpage/\" method=\"post\">
-      <input type=\"hidden\" id=\"MerchantId\" name=\"MerchantId\" value=\"99999994\" >
-      <input type=\"hidden\" id=\"MerchantName\" name=\"MerchantName\" value=\"MerchantName\" >
-      <input type=\"hidden\" id=\"MerchantType\" name=\"MerchantType\" value=\"MerchantType\" >
-      <input type=\"hidden\" id=\"CurrencyCode\" name=\"CurrencyCode\" value=\"$\" >
-      <input type=\"hidden\" id=\"itbis\" name=\"itbis\" value=\"XXX\" >
-      <input type=\"hidden\" id=\"OrderNumber\" name=\"OrderNumber\" value=\"88887771232\" >
-      <input type=\"hidden\" id=\"Amount\" name=\"Amount\" value=\"257597\" >
-      <input type=\"hidden\" id=\"ApprovedUrl\" name=\"ApprovedUrl\" value=\"http://urlderegreso/?ApprovedUrl\" >
-      <input type=\"hidden\" id=\"DeclinedUrl\" name=\"DeclinedUrl\" value=\"http://urlderegreso/?DeclinedUrl\" >
-      <input type=\"hidden\" id=\"CancelUrl\" name=\"CancelUrl\" value=\"http://urlderegreso/?CancelUrl\" >
-      <input type=\"hidden\" id=\"ResponsePostUrl\" name=\"ResponsePostUrl\" value=\" urlderegreso/?PostURL\" >
-      <input type=\"hidden\" id=\"UseCustomField1\" name=\"UseCustomField1\" value=\"1\" >
-      <input type=\"hidden\" id=\"CustomField1Label\" name=\"CustomField1Label\" value=\"Custom1\" >
-      <input type=\"hidden\" id=\"CustomField1Value\" name=\"CustomField1Value\" value=\"Value1\" >
-      <input type=\"hidden\" id=\"UseCustomField2\" name=\"UseCustomField2\" value=\"1\" >
-      <input type=\"hidden\" id=\"CustomField2Label\" name=\"CustomField2Label\" value=\"Custom2\" >
-      <input type=\"hidden\" id=\"CustomField2Value\" name=\"CustomField2Value\" value=\"Value2\" >
-      <input type=\"hidden\" id=\"AuthHash\" name=\"AuthHash\" value=\".......\" >
+    "<form action=\"#{AzulPayButton.config.url_azul}\" method=\"post\">
+      <input type=\"hidden\" id=\"MerchantId\" name=\"MerchantId\" value=\"#{AzulPayButton.config.merchant_id}\" >
+      <input type=\"hidden\" id=\"MerchantName\" name=\"MerchantName\" value=\"#{AzulPayButton.config.merchant_name}\" >
+      <input type=\"hidden\" id=\"MerchantType\" name=\"MerchantType\" value=\"#{AzulPayButton.config.merchant_type}\" >
+      <input type=\"hidden\" id=\"CurrencyCode\" name=\"CurrencyCode\" value=\"#{currency}\" >
+      <input type=\"hidden\" id=\"itbis\" name=\"itbis\" value=\"#{itbis}\" >
+      <input type=\"hidden\" id=\"OrderNumber\" name=\"OrderNumber\" value=\"#{order_number}\" >
+      <input type=\"hidden\" id=\"Amount\" name=\"Amount\" value=\"#{amount}\" >
+      <input type=\"hidden\" id=\"ApprovedUrl\" name=\"ApprovedUrl\" value=\"#{AzulPayButton.config.approved_url}\" >
+      <input type=\"hidden\" id=\"DeclinedUrl\" name=\"DeclinedUrl\" value=\"#{AzulPayButton.config.declined_url}\" >
+      <input type=\"hidden\" id=\"CancelUrl\" name=\"CancelUrl\" value=\"#{AzulPayButton.config.cancel_url}\" >
+      <input type=\"hidden\" id=\"ResponsePostUrl\" name=\"ResponsePostUrl\" value=\"#{AzulPayButton.config.response_post_url}\" >
+      <input type=\"hidden\" id=\"UseCustomField1\" name=\"UseCustomField1\" value=\"#{AzulPayButton.config.custom_field_1}\" >
+      <input type=\"hidden\" id=\"CustomField1Label\" name=\"CustomField1Label\" value=\"#{AzulPayButton.config.custom_field_1_label}\" >
+      <input type=\"hidden\" id=\"CustomField1Value\" name=\"CustomField1Value\" value=\"#{AzulPayButton.config.custom_field_1_value}\" >
+      <input type=\"hidden\" id=\"UseCustomField2\" name=\"UseCustomField2\" value=\"#{AzulPayButton.config.custom_field_2}\" >
+      <input type=\"hidden\" id=\"CustomField2Label\" name=\"CustomField2Label\" value=\"#{AzulPayButton.config.custom_field_2_label}\" >
+      <input type=\"hidden\" id=\"CustomField2Value\" name=\"CustomField2Value\" value=\"#{AzulPayButton.config.custom_field_2_value}\" >
+      <input type=\"hidden\" id=\"AuthHash\" name=\"AuthHash\" value=\"#{Digest::SHA512.hexdigest auth_hash }\" >
       <input type=\"submit\" value=\"Pay with Azul Payment Page\">
     </form>"
 
